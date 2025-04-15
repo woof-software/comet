fef4bf7eb2c6b2c75d4f67a99293eacaac4ae738
Mikhailo Shabodyash <misha.sh@woof.software>
2025-04-15 23:53:24 +0300

    fix

--- deployments/mainnet/weth/relations.ts
+++ deployments/mainnet/weth/relations.ts
@@ -22,6 +22,22 @@ export default {
       }
     }
   },
+  ERC1967Proxy: {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
+  FiatTokenProxy: {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x7050c9e0f4ca769c69bd3a8ef740bc37934f8e2c036e5a723fd8ee048ed3f8c3'
+      }
+    }
+  },
   'TransparentUpgradeableProxy': {
     artifact: 'contracts/ERC20.sol:ERC20',
     delegates: {
