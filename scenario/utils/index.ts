76ec8f3eca46cb93ed93c3925206825c9f65587e
VladyslavPerederWoof <vlad.p@woof.software>
2025-11-17 12:30:56 +0200

    refactor: optimize usesAssetList function by introducing a local variable for comet instance

--- scenario/utils/index.ts
+++ scenario/utils/index.ts
@@ -427,7 +427,8 @@ export async function isRewardSupported(ctx: CometContext): Promise<boolean> {
 }
 
 export async function usesAssetList(ctx: CometContext): Promise<boolean> {
-  return (await (await ctx.getComet()).maxAssets()) === MAX_ASSETS;
+  const comet = await ctx.getComet();
+  return await comet.maxAssets() === MAX_ASSETS;
 }
 
 export function isBridgedDeployment(ctx: CometContext): boolean {
