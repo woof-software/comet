3a4565cae7c4024e51da211d3783cc47324c2766
VladyslavPerederWoof <vlad.p@woof.software>
2025-12-09 11:03:35 +0200

    fix: update migration to return false for enacted status

--- deployments/mantle/usde/migrations/1735299778_update_comet_to_support_more_collaterals.ts
+++ deployments/mantle/usde/migrations/1735299778_update_comet_to_support_more_collaterals.ts
@@ -126,7 +126,7 @@ export default migration('1735299778_update_comet_to_support_more_collaterals', 
   },
 
   async enacted(): Promise<boolean> {
-    return true;
+    return false;
   },
 
   async verify(deploymentManager: DeploymentManager) {
