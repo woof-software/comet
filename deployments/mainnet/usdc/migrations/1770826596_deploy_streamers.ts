0d5f48e281de44592904b6422f4b5f85b0f30741
Mykhailo Shabodyash <misha.sh@woof.software>
2026-02-23 23:20:14 +0200

    wip

--- deployments/mainnet/usdc/migrations/1770826596_deploy_streamers.ts
+++ deployments/mainnet/usdc/migrations/1770826596_deploy_streamers.ts
@@ -14,38 +14,38 @@ const CONSTANTS_PRICE_FEED = '0xD72ac1bCE9177CFe7aEb5d0516a38c88a64cE0AB';
 
 const STREAMER_FACTORY = '0xFB9167A8b5Cb585202953c6d5537A7D640c43a96';
 
-const MAX_REMOVE_FROM_USDC_VAULT = utils.parseUnits('1788506', 6);
+const MAX_REMOVE_FROM_USDC_VAULT = utils.parseUnits('1788800', 6);
 
 const STREAMER_PARAMS = {
   WOOF: {
     streamer: '0xF088339DD8e79819A41aDD5FFB75d9F245AfaAb1',
     recipient: '0xd36025E1e77069aA991DC24f0E6287b4A35c89Ad',
-    remainingStreamAmount: utils.parseUnits('503920.78894', 6),
-    remainingStreamTime: BigNumber.from(7945824),
+    remainingStreamAmount: utils.parseUnits('464283.0416', 6),
+    remainingStreamTime: BigNumber.from(7887439),
   },
   SSP: {
     streamer: '0x334791289a906Ac8f96ac0f90E7A91Bf4AaE4A60',
     recipient: '0xa1fa21665daA59f27046110CC2f58218b6343A2B',
-    remainingStreamAmount: utils.parseUnits('836994.712393', 6),
-    remainingStreamTime: BigNumber.from(15083124),
+    remainingStreamAmount: utils.parseUnits('759218.3771', 6),
+    remainingStreamTime: BigNumber.from(15024739),
   },
   ZERO_SHADOW: {
     streamer: '0xAF9CEE006AE377e88f3BBd668e3d67807F546Bd8',
     recipient: '0x9FAEaBCeD4C29F030d40A83F1a7822624d67f904',
-    remainingStreamAmount: utils.parseUnits('119570.673199', 6),
-    remainingStreamTime: BigNumber.from(15083124),
+    remainingStreamAmount: utils.parseUnits('109083.5419', 6),
+    remainingStreamTime: BigNumber.from(15024739),
   },
   TALLY: {
     streamer: '0x36a0eB84154797DAdCEaCFD046785dB31094C308',
     recipient: '0x7E90E03654732ABedF89Faf87f05BcD03ACEeFdC',
-    remainingStreamAmount: utils.parseUnits('64296.627664', 6),
-    remainingStreamTime: BigNumber.from(13517724),
+    remainingStreamAmount: utils.parseUnits('62527.0873', 6),
+    remainingStreamTime: BigNumber.from(13459339),
   },
   GAUNTLET: {
     streamer: '0xEA2B6BC719CF6D2Fed07865d26987D32d570DbBD',
     recipient: '0xD20c9667bf0047F313228F9fE11F8b9F8Dc29bBa',
-    remainingStreamAmount: utils.parseUnits('964230.664321', 6),
-    remainingStreamTime: BigNumber.from(18886944),
+    remainingStreamAmount: utils.parseUnits('813373.3687', 6),
+    remainingStreamTime: BigNumber.from(18828559),
   }
 };
 
@@ -101,8 +101,30 @@ async function deployStreamer(deploymentManager: DeploymentManager, recipient: s
 }
 
 export default migration('1770826596_deploy_streamers', {
-  async prepare() {
-    return {};
+  async prepare(deploymentManager: DeploymentManager) {
+
+    const woofParams = STREAMER_PARAMS.WOOF;
+    const sspParams = STREAMER_PARAMS.SSP;
+    const zeroShadowParams = STREAMER_PARAMS.ZERO_SHADOW;
+    const tallyParams = STREAMER_PARAMS.TALLY;
+    const gauntletParams = STREAMER_PARAMS.GAUNTLET;
+    console.log('WOOF:');
+    woofStreamer = await deployStreamer(deploymentManager, woofParams.recipient, woofParams.remainingStreamAmount, woofParams.remainingStreamTime);
+    console.log('SSP:');
+    sspStreamer = await deployStreamer(deploymentManager, sspParams.recipient, sspParams.remainingStreamAmount, sspParams.remainingStreamTime);
+    console.log('Zero Shadow:');
+    zeroShadowStreamer = await deployStreamer(deploymentManager, zeroShadowParams.recipient, zeroShadowParams.remainingStreamAmount, zeroShadowParams.remainingStreamTime);
+    console.log('Tally:');
+    tallyStreamer = await deployStreamer(deploymentManager, tallyParams.recipient, tallyParams.remainingStreamAmount, tallyParams.remainingStreamTime);
+    console.log('Gauntlet:');
+    gauntletStreamer = await deployStreamer(deploymentManager, gauntletParams.recipient, gauntletParams.remainingStreamAmount, gauntletParams.remainingStreamTime);
+    return {
+      woofStreamer,
+      sspStreamer,
+      zeroShadowStreamer,
+      tallyStreamer,
+      gauntletStreamer
+    };
   },
 
   async enact(deploymentManager: DeploymentManager) {
@@ -120,16 +142,16 @@ export default migration('1770826596_deploy_streamers', {
     const tallyParams = STREAMER_PARAMS.TALLY;
     const gauntletParams = STREAMER_PARAMS.GAUNTLET;
 
-    console.log('WOOF:');
-    woofStreamer = await deployStreamer(deploymentManager, woofParams.recipient, woofParams.remainingStreamAmount, woofParams.remainingStreamTime);
-    console.log('SSP:');
-    sspStreamer = await deployStreamer(deploymentManager, sspParams.recipient, sspParams.remainingStreamAmount, sspParams.remainingStreamTime);
-    console.log('Zero Shadow:');
-    zeroShadowStreamer = await deployStreamer(deploymentManager, zeroShadowParams.recipient, zeroShadowParams.remainingStreamAmount, zeroShadowParams.remainingStreamTime);
-    console.log('Tally:');
-    tallyStreamer = await deployStreamer(deploymentManager, tallyParams.recipient, tallyParams.remainingStreamAmount, tallyParams.remainingStreamTime);
-    console.log('Gauntlet:');
-    gauntletStreamer = await deployStreamer(deploymentManager, gauntletParams.recipient, gauntletParams.remainingStreamAmount, gauntletParams.remainingStreamTime);
+    // console.log('WOOF:');
+    // woofStreamer = await deployStreamer(deploymentManager, woofParams.recipient, woofParams.remainingStreamAmount, woofParams.remainingStreamTime);
+    // console.log('SSP:');
+    // sspStreamer = await deployStreamer(deploymentManager, sspParams.recipient, sspParams.remainingStreamAmount, sspParams.remainingStreamTime);
+    // console.log('Zero Shadow:');
+    // zeroShadowStreamer = await deployStreamer(deploymentManager, zeroShadowParams.recipient, zeroShadowParams.remainingStreamAmount, zeroShadowParams.remainingStreamTime);
+    // console.log('Tally:');
+    // tallyStreamer = await deployStreamer(deploymentManager, tallyParams.recipient, tallyParams.remainingStreamAmount, tallyParams.remainingStreamTime);
+    // console.log('Gauntlet:');
+    // gauntletStreamer = await deployStreamer(deploymentManager, gauntletParams.recipient, gauntletParams.remainingStreamAmount, gauntletParams.remainingStreamTime);
 
     const totalSumForAllStreamers = woofParams.remainingStreamAmount
       .add(sspParams.remainingStreamAmount)
