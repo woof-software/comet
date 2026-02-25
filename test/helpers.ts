2fabda5b3cb569906f8f7857745c84a6ea6d8aac
VladyslavPerederWoof <vlad.p@woof.software>
2026-02-25 10:43:44 +0200

    wip

--- test/helpers.ts
+++ test/helpers.ts
@@ -44,7 +44,7 @@ import { TotalsBasicStructOutput, TotalsCollateralStructOutput } from '../build/
 // Snapshot
 import { takeSnapshot, SnapshotRestorer } from './helpers/snapshot';
 
-export { Comet, ethers, expect, hre, takeSnapshot, SnapshotRestorer };
+export { Comet, ethers, expect, hre, takeSnapshot, SnapshotRestorer, SignerWithAddress, BigNumber };
 
 export type Numeric = number | bigint;
 
@@ -174,6 +174,20 @@ export function truncateDecimals(factor: bigint | BigNumber, decimals = 4) {
 
 export function mulPrice(n: bigint, price: bigint | BigNumber, fromScale: bigint | BigNumber): bigint {
   return n * toBigInt(price) / toBigInt(fromScale);
+}
+
+const BASE_INDEX_SCALE = BigInt(1e15);
+
+export function presentValue(principalValue: bigint, baseSupplyIndex: bigint, baseBorrowIndex: bigint): bigint {
+  if (principalValue >= 0n) {
+    return principalValue * baseSupplyIndex / BASE_INDEX_SCALE;
+  } else {
+    return -((-principalValue) * baseBorrowIndex / BASE_INDEX_SCALE);
+  }
+}
+
+export function mulFactor(n: bigint, factor: bigint | BigNumber): bigint {
+  return n * toBigInt(factor) / toBigInt(factorScale);
 }
 
 function toBigInt(f: bigint | BigNumber): bigint {
