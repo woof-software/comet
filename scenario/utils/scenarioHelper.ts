4e64b79811359ad99be1f8ee9d0f930f161145d1
Mykhailo Shabodyash <misha.sh@woof.software>
2026-06-02 16:47:15 +0300

    fix: config for mainnet usdc scenarios

--- scenario/utils/scenarioHelper.ts
+++ scenario/utils/scenarioHelper.ts
@@ -74,6 +74,14 @@ export function getConfigForScenario(ctx: CometContext, i?: number) {
     config.liquidationAsset = 100;
   }
 
+  if (ctx.world.base.network === 'mainnet' && ctx.world.base.deployment === 'usdc') {
+    config.bulkerBase = 1000;
+    config.bulkerAsset = 100;
+    config.bulkerComet = 500;
+    config.bulkerBorrowBase = 101;
+    config.bulkerBorrowAsset = 1;
+  }
+
   if (ctx.world.base.network === 'mainnet' && ctx.world.base.deployment === 'usdt') {
     if(i == 12) {
       config.supplyCollateral = 0;
