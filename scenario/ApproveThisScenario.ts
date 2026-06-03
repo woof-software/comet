13e91d2c04da9dc19fee0b0115a33b846c2ed3ed
anik-woof <anna.shr@woof.software>
2026-06-03 18:20:37 +0100

    test: cover approveThis initial state and bad amount

--- scenario/ApproveThisScenario.ts
+++ scenario/ApproveThisScenario.ts
@@ -6,6 +6,8 @@ import { constants } from 'ethers';
 scenario('Comet#approveThis > allows governor to authorize and rescind authorization for Comet ERC20', {}, async ({ comet, timelock, actors }, context) => {
   const { admin } = actors;
 
+  expect(await comet.isAllowed(comet.address, timelock.address)).to.be.false;
+
   await context.setNextBaseFeeToZero();
   await admin.approveThis(timelock.address, comet.address, constants.MaxUint256, { gasPrice: 0 });
 
@@ -22,6 +24,8 @@ scenario('Comet#approveThis > allows governor to authorize and rescind authoriza
   const baseTokenAddress = await comet.baseToken();
   const baseToken = context.getAssetByAddress(baseTokenAddress);
 
+  expect(await baseToken.allowance(comet.address, timelock.address)).to.be.equal(0n);
+
   const newAllowance = 999_888n;
   await context.setNextBaseFeeToZero();
   await admin.approveThis(timelock.address, baseTokenAddress, newAllowance, { gasPrice: 0 });
@@ -32,6 +36,16 @@ scenario('Comet#approveThis > allows governor to authorize and rescind authoriza
   await admin.approveThis(timelock.address, baseTokenAddress, 0, { gasPrice: 0 });
 
   expect(await baseToken.allowance(comet.address, timelock.address)).to.be.equal(0n);
+});
+
+scenario('Comet#approveThis > reverts if Comet ERC20 amount is not 0 or uint256.max', {}, async ({ comet, timelock, actors }, context) => {
+  const { admin } = actors;
+
+  await context.setNextBaseFeeToZero();
+  await expectRevertCustom(
+    admin.approveThis(timelock.address, comet.address, 300, { gasPrice: 0 }),
+    'BadAmount()'
+  );
 });
 
 scenario('Comet#approveThis > reverts if not called by governor', {}, async ({ comet, timelock }) => {
