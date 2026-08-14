d3e0656528ccd03440679f860213cc21f2c1ddec
anik-woof <anna.shr@woof.software>
2026-08-14 11:48:32 +0100

    fix: handle null values in fuzzing requirements

--- scenario/constraints/Fuzzing.ts
+++ scenario/constraints/Fuzzing.ts
@@ -89,7 +89,7 @@ export function getFuzzedRequirements(requirements: Requirements): Requirements[
     if (isFuzzConfig(value)) {
       let fuzzedValues = getFuzzedValues(value);
       keyValues.push(fuzzedValues.map(v => ({ key, value: v.toString() })));
-    } else if (typeof value === 'object' && typeof value !== null) {
+    } else if (typeof value === 'object' && value !== null) {
       // If value is a non-null object, recursively fuzz its properties
       keyValues.push(getFuzzedRequirements(value).map(v => ({ key, value: v})));
     } else {
