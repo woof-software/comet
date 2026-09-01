eecad187ee0a3f1bc97d348925ffb8c621cbfe3e
VladyslavBudiukWOOF <vlad.b@woof.software>
2026-09-01 22:06:22 +0200

    fix: removed the unused code

--- scenario/utils/index.ts
+++ scenario/utils/index.ts
@@ -1577,18 +1577,11 @@ export async function executeOpenProposalAndRelay(
 
   for (const bridgeManager of bridgeManagers) {
     await mockAllRedstoneOracles(bridgeManager);
-    if (bridgeManager) {
       await relayMessage(
         governanceDeploymentManager,
         bridgeManager,
         startingBlockNumber
       );
-    } else {
-      console.log(
-        `[${governanceDeploymentManager.network} -> ${bridgeManager.network}] Proposal ${openProposal.id} doesn't target bridge; not relaying`
-      );
-      return;
-    }
   }
 }
 
