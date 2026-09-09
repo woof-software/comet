88d5fda913181e6d20cd9e18aa468c8b2478d1f8
Mykhailo Shabodyash <misha.sh@woof.software>
2026-09-09 12:07:58 +0300

    fix: pr link

--- deployments/linea/weth/migrations/1788887942_update_wrseth_pricefeed_to_chainlink.ts
+++ deployments/linea/weth/migrations/1788887942_update_wrseth_pricefeed_to_chainlink.ts
@@ -92,11 +92,11 @@ export default migration('1788887942_update_wrseth_pricefeed_to_chainlink', {
 
 Woof proposes to update wrsETH's price feed on cWETHv3 on the Linea network from the deprecated Kelp oracle to a new Chainlink price feed.
 
-In order to achieve this, wrsETH's price feed will be updated to a new price feed contract with underlying Chainlink ([oracle](https://lineascan.build/address/0xEEDF0B095B5dfe75F3881Cb26c19DA209A27463a#readContract)).
+In order to achieve this, wrsETH's price feed will be updated to a new price feed contract with underlying Chainlink ([oracle](https://lineascan.build/address/0xEEDF0B095B5dfe75F3881Cb26c19DA209A27463a)).
 
 This proposal takes the governance steps recommended and necessary to update a Compound III WETH market on Linea. Simulations have confirmed the market's readiness, as much as possible, using the [Comet scenario suite](https://github.com/compound-finance/comet/tree/main/scenario).
 
-Further detailed information can be found on the corresponding [proposal pull request](https://github.com/compound-finance/comet/pull/22).
+Further detailed information can be found on the corresponding [proposal pull request](https://github.com/Compound-Foundation/comet/pull/23).
 
 
 ## Proposal Actions
