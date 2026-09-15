dce4bec6b2b7c3d8983c1f03ecc12823bbe435da
VladyslavPerederWoof <vlad.p@woof.software>
2026-09-15 17:55:22 +0300

    fix: forge tests running

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -67,13 +67,13 @@ const {
   ETHERSCAN_KEY_FOR_LINEA,
   SNOWTRACE_KEY,
   MAINNET_QUICKNODE_LINK,
-  SEPOLIA_QUICKNODE_LINK,
   RONIN_QUICKNODE_LINK,
   POLYGON_QUICKNODE_LINK,
   OPTIMISM_QUICKNODE_LINK,
   MANTLE_QUICKNODE_LINK,
   BASE_QUICKNODE_LINK,
   ARBITRUM_QUICKNODE_LINK,
+  SCROLL_QUICKNODE_LINK,
   UNICHAIN_QUICKNODE_LINK = '',
   LINEA_QUICKNODE_LINK = '',
   MNEMONIC = 'myth like woof scare over problem client lizard pioneer submit female collect',
@@ -126,11 +126,6 @@ export const networkConfigs: NetworkConfig[] = [
     network: 'mainnet',
     chainId: 1,
     url: `${MAINNET_QUICKNODE_LINK}`,
-  },
-  {
-    network: 'sepolia',
-    chainId: 11155111,
-    url: `${SEPOLIA_QUICKNODE_LINK}`,
   },
   {
     network: 'ronin',
@@ -188,7 +183,7 @@ export const networkConfigs: NetworkConfig[] = [
   {
     network: 'scroll',
     chainId: 534352,
-    url: 'https://scroll.drpc.org',
+    url: `${SCROLL_QUICKNODE_LINK}`,
   },
 ];
 
@@ -373,7 +368,7 @@ const config: HardhatUserConfig = {
       mantle: ETHERSCAN_KEY,
       unichain: ETHERSCAN_KEY,
       // Scroll
-      'scroll': ETHERSCAN_KEY,
+      scroll: ETHERSCAN_KEY,
       linea: ETHERSCAN_KEY_FOR_LINEA,
     },
     customChains: [
