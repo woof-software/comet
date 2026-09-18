9625ea2fe7ce4cd14780ce164c85ab74061ba5a9
VladyslavPerederWoof <vlad.p@woof.software>
2026-09-18 10:51:24 +0300

    test: increase tolerance for lastUpdated time check in supply tests from 2 to 5 seconds

--- test/supply-test.ts
+++ test/supply-test.ts
@@ -846,7 +846,7 @@ describe('supply', function () {
           (await ethers.provider.getBlock((await supplyTx.wait()).blockNumber)).timestamp
         );
 
-        expect(lastUpdated - cometUpdatedTimeBefore).to.be.approximately(SKIP_TIME, 2); // 2 seconds tolerance
+        expect(lastUpdated - cometUpdatedTimeBefore).to.be.approximately(SKIP_TIME, 5); // 5 seconds tolerance
         expect(lastUpdated).to.equal(supplyTimestamp);
       });
 
